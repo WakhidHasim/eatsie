9e21ad8116691c2f5fb8ef1faca2f6e98fbd573f
WakhidHasim <wakhidhasim2000@gmail.com>
2022-11-24 16:24:09 +0700

    fragment done

--- app/src/main/java/kelompok4/eatsie/DashboardActivity.kt
+++ app/src/main/java/kelompok4/eatsie/DashboardActivity.kt
@@ -2,10 +2,38 @@ package kelompok4.eatsie
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
+import kelompok4.eatsie.databinding.ActivityDashboardBinding
+
 
 class DashboardActivity : AppCompatActivity() {
+    lateinit var binding: ActivityDashboardBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_dashboard)
+        binding = ActivityDashboardBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        replaceFragment(Home())
+
+        binding.bottomNavigationView.setOnItemSelectedListener {
+            when(it.itemId){
+                R.id.itemMenuHome -> replaceFragment(Home())
+                R.id.itemMenuFavorites -> replaceFragment(Favorites())
+                R.id.itemMenuProfile -> replaceFragment(Profile())
+
+                else ->{
+
+                }
+            }
+            true
+        }
+    }
+
+    private fun replaceFragment (fragment: Fragment) {
+        val fragmentManager = supportFragmentManager
+        val fragmentTransaction = fragmentManager.beginTransaction()
+
+        fragmentTransaction.replace(R.id.frameLayoutContainer, fragment)
+        fragmentTransaction.commit()
     }
 }
